c0c53457821eac260118f861e3caafd0b46b3994
Obiageli-Ann <spicyann001@gmail.com>
2024-03-05 22:14:14 +0100

    Added functionality using API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
+function weatherUpdate (response){
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  
+}
+
+function searchCity(city){
+  let apiKey = "9f0940fbf3ca3obdd306d133a0041aft";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+ axios.get(apiUrl).then(weatherUpdate);
+}
+
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let inputForm = document.querySelector("#input-form");
 let cityElement=document.querySelector("#city");
 cityElement.innerHTML=inputForm.value
+searchCity(inputForm.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
